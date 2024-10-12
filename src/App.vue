6ea20a7f0cc35f8fd1885720ffd1e6261d2466e9
Hwang-gyu-jin <dhee332123@gmail.com>
2024-10-12 15:34:36 +0900

    로그인 페이지 제작

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <component :is="Component" />
       </transition>
     </router-view>
-        <BottomNav />
+    <BottomNav v-show="currentState === 'BotNav'"/>
   </div>
 </template>
 
@@ -23,21 +23,35 @@ export default {
   components: {
     BottomNav,
   },
+  computed: {
+    // 현재 라우트의 meta 정보를 가져오기 위한 computed 속성
+    currentState() {
+      console.log('Current Route Meta State:', this.$route.meta.state);
+      return this.$route.meta.state; // 현재 라우트의 meta.state 값을 반환
+    }
+  },
   watch: {
-    $route(to, from) {
-      this.transitionName = to.meta.page > from.meta.page ? "next" : "prev";
+    // $route(to, from) {
+    //   this.transitionName = to.meta.page > from.meta.page ? "next" : "prev";
 
-      console.log(this.transitionName);
-    },
+    //   console.log(this.transitionName);
+    // },
   },
 };
 </script>
 
 <style>
+
+@font-face {
+  font-family: 'GwangYang'; /* 폰트 이름 */
+  src: url('/public/assets/fonts/GWANGYANG-REGULAR.TTF') format('truetype'); /* 폰트 경로 */    /* 폰트 경로 */
+  font-weight: normal;
+  font-style: normal;
+}
+
 #app {
   display: flex;
-  flex-direction: row;
-  align-items: center; /* 수직 중앙 정렬 */
+  flex-direction: column;
   margin: 0;
   padding: 0;
 }
@@ -48,5 +62,6 @@ body {
   padding: 0;
   height: 100%;
   overflow-x: hidden;
+  font-family: 'GwangYang', sans-serif;
 }
 </style>
