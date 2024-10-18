a599c644e90cd39de637a44f72857e2b5209adf9
Hwang-gyu-jin <dhee332123@gmail.com>
2024-10-18 21:29:54 +0900

    달력

---
+++ src/components/MediCalendarPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="medManage">
+    <h1>medManage Page</h1>
+    <p>medManage .테스트 화면입니다</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MedManagePage'
+};
+</script>
+
+<style>
+.medManage {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: yellow;
+}
+</style>
