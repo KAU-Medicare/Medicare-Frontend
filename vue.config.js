25e7493cd60cbbe18b2c3a23cc572a2e328fd0a6
Hwang-gyu-jin <dhee332123@gmail.com>
2024-10-21 17:34:11 +0900

    로그인 테스트

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,6 @@ module.exports = defineConfig({
       '/api': {
         target: 'https://kau-medicare.shop', // Spring Boot 서버 주소
         changeOrigin: true,
-        pathRewrite: { '^/api': '' } // 필요에 따라 경로 재작성
       }
     }
   }
