f3511652314851c74f7b73b1044375e985dc0a0e
Hwang-gyu-jin <dhee332123@gmail.com>
2024-11-07 00:55:38 +0900

    새 작업용 변경사항

---
+++ src/components/AllergyInfResultPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="allergyInfResult">
+    <h1>AllergyInfPage</h1>
+    <p>Your AllergyInf will be displayed here.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AllergyInfResultPage",
+};
+</script>
+
+<style scoped>
+.allergyInfResult {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: green;
+}
+
+</style>
